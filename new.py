3b4bcc2c0abd9ea3444d7d65679698c85c6936a6
xDshadowop <104053807+xDshadowop@users.noreply.github.com>
2022-05-09 22:18:14 +0530

    Update new.py

--- new.py
+++ new.py
@@ -15,10 +15,17 @@ print (k)
 
 
 '''
-
+'''
 async for x in bot.iter_messages(event.chat_id):
   if x.text.startswith("Shadow"):
     await bot.forward_messages(event.chat_id, x.id, event.chat_id)
     break
   else:
     continue
+'''
+ok = 0
+async for x in bot.iter_dialogs(limit=5):
+   try:
+      await bot.send_message(x.id, "hello "+str(ok))
+      ok+1
+   execpt pass
